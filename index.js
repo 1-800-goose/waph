6db210c2b672de0692fb7da7921417774c7c6afb
robinluo <robinluo@uw.edu>
2021-11-10 22:55:46 -0800

    changed select to 1

--- index.js
+++ index.js
@@ -24,8 +24,8 @@
 
   function add() {
     let raph = document.createElement("img");
-    let select = Math.floor(3 * Math.random());
-    raph.src = "img/raph" + select + ".png";
+    //let select = Math.floor(3 * Math.random());
+    raph.src = "img/raph" + 1 + ".png";
     raph.alt = "raphael meng being a hot n sexy himbo :)";
     if(id("photos").childNodes.length==0) {
       id("photos").appendChild(raph);
